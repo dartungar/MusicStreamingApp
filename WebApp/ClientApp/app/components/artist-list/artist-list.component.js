68e6c0aa0784574972068eb66bc30faad0a8e2e7
danila nikolayev <d.nikolayev@treolan.ru>
2021-07-20 18:13:38 +0300

    implemented list view of Artists with DevExpress component

--- WebApp/ClientApp/app/components/artist-list/artist-list.component.js
+++ WebApp/ClientApp/app/components/artist-list/artist-list.component.js
@@ -3,26 +3,54 @@
         module('clientApp').
         component('artistList', {
             
-            controller: function artistListController(artistService, $http) {
+            controller: function artistListController(artistService, $http, $scope) {
                 var self = this;
                 self.artists = [];
+                var store = new DevExpress.data.CustomStore({
+                    key: "id",
+                    load: function () {
+                        return artistService.getList().then(
+                            // on success, set state to received data
+                            function (res) {
+                                console.log('got data', res.data);
+                                return {
+                                    data: res.data
+                                }
+                            },
+                            // on fail, just console.log
+                            function () {
+                                console.log('Error receiving data')
+                            }
+                        );
+                    },
+                })
 
-                self.loadArtists = function () {
-                    artistService.getList().then(
-                        // on success, set state to received data
-                        function (res) {
-                            console.log(res.data);
-                            self.artists = res.data;
+                $scope.dataGridOptions = {
+                    dataSource: store,
+                    remoteOperations: false,
+                    columns: [
+                        {
+                            dataField: "name",
+                            dataType: "string"
                         },
-                        // on fail, just console.log
-                        function () {
-                            console.log('Error receiving data')
-                        }
-                    );
+                        {
+                            dataField: "description",
+                            dataType: "string"
+                        },
+                        {
+                            dataField: "facebookLink",
+                            dataType: "string"
+                        },
+                    ],
+                    searchPanel: {
+                        visible: true,
+                        highlightCaseSensitive: true
+                    },
                 }
 
-                self.loadArtists();
-
+                //self.loadArtists();
+                setTimeout(() => console.log(store), 5000)
+                
             },
             templateUrl: "/components/artist-list/artist-list.template.html" 
         });
